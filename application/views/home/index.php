ea5c76ebc0df27358f5da2a8541812cd4c9dae54
Felipe Jung <felipemattoseu@gmail.com>
2021-04-24 13:27:27 -0400

    Adiciona botoes da pagina inicial

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -3,5 +3,36 @@
 } ?>
 
 <div class="container">
-    <h1>Home</h1>
+    <div>
+        <h2>Escolha uma das opções:</h2>
+        <div class="optionsWrapper">
+            <button class="btn">
+                Cidades
+            </button>
+            <button class="btn">
+                Filmes
+            </button>
+            <button class="btn">
+                Equipe
+            </button>
+            <button class="btn">
+                Comidas
+            </button>
+            <button class="btn">
+                Unidades
+            </button>
+            <button class="btn">
+                Combos
+            </button>
+            <button class="btn">
+                Clientes
+            </button>
+            <button class="btn">
+                Promoções
+            </button>
+            <button class="btn">
+                Sessões
+            </button>
+        </div>
+    </div>
 </div>
