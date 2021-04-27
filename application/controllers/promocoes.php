c4e8ea7b3886da89853b12dc2358b50ce3f80c43
Felipe Jung <felipemattoseu@gmail.com>
2021-04-27 08:35:26 -0400

    Arruma controller de promoções

--- application/controllers/promocoes.php
+++ application/controllers/promocoes.php
@@ -1,17 +1,17 @@
 <?php
 
 
-class Combos
+class Promocoes
 {
   /**
    * PAGE: index
-   * Este método é acessado ao acessar -> http://localhost/cineweb/index.php/combos
+   * Este método é acessado ao acessar -> http://localhost/cineweb/index.php/promocoes
    */
   public function index()
   {
     // load views
     require APP . 'views/_templates/header.php';
-    require APP . 'views/combos/index.php';
+    require APP . 'views/promocoes/index.php';
     require APP . 'views/_templates/footer.php';
   }
 }
