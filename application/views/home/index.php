ff4989c1da6aaa45cf61158da12603dc6ec764e7
Felipe Jung <felipemattoseu@gmail.com>
2021-04-27 08:36:03 -0400

    Ajusta links da tela inicial

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -6,31 +6,32 @@
     <div>
         <h2>Olá! Escolha uma das opções:</h2>
         <div class="optionsWrapper">
-            <a class="btn homeBtn" href="<?php echo URL_WITH_INDEX_FILE; ?>/cidades">
+            <a class="btn homeBtn" href="<?php echo URL_WITH_INDEX_FILE; ?>cidades">
                 Cidades
             </a>
-            <a class="btn homeBtn">
+            <a class="btn homeBtn" href="<?php echo URL_WITH_INDEX_FILE; ?>filmes">
                 Filmes
             </a>
-            <a class="btn homeBtn">
-                Equipe
+            <a class="btn homeBtn" href="<?php echo URL_WITH_INDEX_FILE; ?>funcionarios">
+                Funcionários
             </a>
-            <a class="btn homeBtn">
+            <a class="btn homeBtn" href="<?php echo URL_WITH_INDEX_FILE; ?>comidas">
                 Comidas
             </a>
-            <a class="btn homeBtn">
+            <a class="btn homeBtn" href="<?php echo URL_WITH_INDEX_FILE; ?>unidades">
                 Unidades
             </a>
-            <a class="btn homeBtn">
+            <a class="btn homeBtn" href="<?php echo URL_WITH_INDEX_FILE; ?>combos">
                 Combos
             </a>
-            <a class="btn homeBtn">
+
+            <a class="btn homeBtn" href="<?php echo URL_WITH_INDEX_FILE; ?>clientes">
                 Clientes
             </a>
-            <a class="btn homeBtn">
+            <a class="btn homeBtn" href="<?php echo URL_WITH_INDEX_FILE; ?>promocoes">
                 Promoções
             </a>
-            <a class="btn homeBtn">
+            <a class="btn homeBtn" href="<?php echo URL_WITH_INDEX_FILE; ?>sessoes">
                 Sessões
             </a>
         </div>
